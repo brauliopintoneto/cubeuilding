ca5d1f285847edc652d6c3553ed1a0f27f000ecf
Daniel Ibrahim <danielibrahimxxx@gmail.com>
2015-10-06 16:42:14 -0400

    Adding skybox and color to walls

---
+++ Assets/_Scripts/Cube.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3(
+			-Input.GetAxis("Horizontal") * Time.deltaTime * 1,
+			0,
+			-Input.GetAxis("Vertical") * Time.deltaTime * 1));
+	}
+}
