fcb16b6dc8a8ef0b814b2a5029f113348c833c40
Daniel Ibrahim <danielibrahimxxx@gmail.com>
2015-10-07 16:05:16 -0400

    Adding score support

---
+++ Assets/_Scripts/ScoreController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour {
+
+	public Text scoreText;
+
+	private long score;
+
+	public void AddScore (long scoreToAdd) {
+		score += scoreToAdd;
+		UpdateScore ();
+	}
+
+	public void UpdateScore () {
+		scoreText.text = "SCORE: " + score;
+	}
+
+}
---
+++ Assets/_Scripts/StageCenterBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageCenterBehaviour : MonoBehaviour {
+
+	private ScoreController scoreController;
+
+	void Awake() {
+		scoreController = GameObject.FindGameObjectWithTag ("ScoreController")
+			.GetComponent<ScoreController>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter (Collision col) {
+		scoreController.AddScore (10);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
