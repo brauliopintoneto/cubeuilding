3b3ca3f2ede7948985f29ab1a6b2a81db5710d6c
luan francisco lima fernandes <luanfranciscojr@gmail.com>
2015-10-12 09:20:33 -0700

    tela inicial

---
+++ Assets/gameMainController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameMainController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("space"))
+        {
+            Application.LoadLevel("Game");
+        }
+	
+	}
+}
